baa3e5d155fa057b3499821231e016f1a050e6e5
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-01 18:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,15 @@
 
 function palindrome(str){
 
+ const string = str.toLowerCase();
+ const len = str.length;
+ 
+        for (let i = 0; i < len / 2; i++) {
+         if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
+
